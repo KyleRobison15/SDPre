2403e2d0ee4968d0c1d16b16f9bce64835cebeb6
Kyle Robison <kylerobison15@gmail.com>
2021-04-13 17:13:13 -0600

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,11 @@
+public class Objective2Lab2 {
+  public static void main (String[] args) {
+    String name = "Kyle Robison";
+    int age = 27;
+    String state = "Colorado";
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+
+  }
+}
