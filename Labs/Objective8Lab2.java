e5fae6af1e5ea4b3eb18d4c221db31c2b1198575
Kyle Robison <kylerobison15@gmail.com>
2021-04-17 15:13:28 -0600

    complete objective8labs1-3

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+  public static void main (String[] args) {
+    int total = 0;
+    for (int count = 0;count <= 20;count++) {
+      total = total + count;
+    }
+    System.out.println(total);
+  }
+}
