52f8ddc7da4f0f144499a542c95b6863d4375f2e
Kyle Robison <kylerobison15@gmail.com>
2021-04-17 16:19:51 -0600

    MethodsExample

---
+++ Labs/MethodsExample.java
@@ -0,0 +1,17 @@
+public class MethodsExample {
+  public static void main (String[] args) {
+    printSquare();
+    printRectangle();
+
+  }
+  public static void printSquare() {
+    for (int line = 1;line <= 10;line++ ) {
+      System.out.println("XXXXXXXXXXXXXXXXX");
+    }
+  }
+  public static void printRectangle() {
+    for (int line = 1;line <= 20;line++ ) {
+      System.out.println("XXXXXXXXXXXXXXXXX");
+    }
+  }
+}
